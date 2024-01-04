4b87cd1fe8d2b430d5095f6108ca197df64685b2
Daniel Brutti <danielbrutti@gmail.com>
2024-01-04 12:26:42 -0300

    Validation for command values. Migration to AWS SDK v3.

--- packages/core/src/services/iot/iot_control.ts
+++ packages/core/src/services/iot/iot_control.ts
@@ -1,22 +1,26 @@
 export * as IoTControl from "./iot_control";
-import AWS from "aws-sdk";
+import { IoTDataPlaneClient, PublishCommand } from "@aws-sdk/client-iot-data-plane";
 import { Config } from "sst/node/config";
 
-const iotdata = new AWS.IotData({ endpoint: Config.IOT_ENDPOINT });
+const client = new IoTDataPlaneClient({
+  endpoint: `https://${Config.IOT_ENDPOINT}`
+});
 
 export async function sendCommand(bot_uuid: string, pdu_outlet_number: number, command: string): Promise<boolean> {
-  const params = {
+  const payload = JSON.stringify({
+    "outlet_id": pdu_outlet_number,
+    "command": command
+  });
+
+  const publishCommand = new PublishCommand({
     topic: `chargebot/control/${bot_uuid}/outlet`,
-    payload: JSON.stringify({
-      "outlet_id": pdu_outlet_number,
-      "command": command
-    }),
+    payload: Buffer.from(payload),
     qos: 1
-  };
+  })
 
   try {
-    await iotdata.publish(params).promise();
-    return true;
+    const response = await client.send(publishCommand);
+    return response.$metadata.httpStatusCode === 200;
   } catch (err) {
     console.error('Error publishing message:', err);
     return false;
--- packages/functions/src/schemas/control_outlet.schema.ts
+++ packages/functions/src/schemas/control_outlet.schema.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const ControlOutletSchema = Joi.object({
   pdu_outlet_number: Joi.number().required(),
-  command: Joi.allow(
+  command: Joi.string().valid(
     'start_priority_charge',
     'stop_priority_charge'
   ).required(),
